46259309b82d7d42c51e404d0c47bcb5699a414f
Animesh Sahu <animeshsahu19@yahoo.com>
2024-02-05 22:12:27 +0530

    fixup(cp/template.cpp): allow printing map<T, vector<V>>

--- cp/template.cpp
+++ cp/template.cpp
@@ -47,10 +47,10 @@ string binary(int n) {
 
 template <class T> void _print(T t) {cerr << t;}
 void _print(string t) {cerr << '"' << t << '"';}
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]"<<endl;}
 template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
 template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
 template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 
